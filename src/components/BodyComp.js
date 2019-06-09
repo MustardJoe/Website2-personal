b51a82d8bb2a092d09f6019945b25b0c4b87b88f
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-08 21:39:22 -0700

    added nav bar comp to bodyComp component

--- src/components/BodyComp.js
+++ src/components/BodyComp.js
@@ -1,16 +1,20 @@
 import Component from './Component.js';
 import Bio from './Bio.js';
+import NavBar from './NavBar.js';
 
 class BodyComp extends Component {    
     render() {
         const dom = this.renderDOM();
+
+        const navBar = new NavBar();
+        dom.prepend(navBar.render());
 
         const bio = new Bio();
         dom.appendChild(bio.render());
 
         return dom;
     }
-    
+
     renderTemplate() {
         return /*html*/`
             <div class="body-comp">Hello World!</div>
