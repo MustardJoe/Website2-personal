ed3da77a2d52f4ef0d4fd110eccb8c4d9cbc057a
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-08 20:55:33 -0700

    created bio component

---
+++ src/components/Bio.js
@@ -0,0 +1,11 @@
+import Component from './Component.js';
+
+class Bio extends Component {
+    renderTemplate() {
+        return /*html*/`
+        <div>Hi I’m Jon!  Here’s a brief introduction to me, my interests and projects.</div>
+    `;
+    }
+}
+
+export default Bio;
