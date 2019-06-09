24b351cf9c456c02f49351b54db4ff9bae2297fb
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-08 22:55:47 -0700

    added hidden class to bio comp for dynamic update of page

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,7 +3,7 @@ import Component from './Component.js';
 class Bio extends Component {
     renderTemplate() {
         return /*html*/`
-        <div>Hi I’m Jon!  Here’s a brief introduction to me, my interests and projects.</div>
+        <div class="hidden">Hi I’m Jon!  Here’s a brief introduction to me, my interests and projects.</div>
     `;
     }
 }
