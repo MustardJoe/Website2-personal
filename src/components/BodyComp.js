95ab112c02dc0cc4c50197dc323a90b2c46df4f6
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-08 20:56:04 -0700

    rendered bio component in bodycomp comp

--- src/components/BodyComp.js
+++ src/components/BodyComp.js
@@ -1,6 +1,16 @@
 import Component from './Component.js';
+import Bio from './Bio.js';
 
 class BodyComp extends Component {    
+    render() {
+        const dom = this.renderDOM();
+
+        const bio = new Bio();
+        dom.appendChild(bio.render());
+
+        return dom;
+    }
+    
     renderTemplate() {
         return /*html*/`
             <div class="body-comp">Hello World!</div>
