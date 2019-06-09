c386dd76bad4de5e0e156659dbeff3d47df69daa
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-09 03:39:04 -0700

    created home component

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import Component from './Component.js';
+
+class Home extends Component {
+    renderTemplate() {
+        return /*html*/`
+        <section class="home-comp">
+            <div >
+                Hi I’m Jon!  Welcome to my internet home (away from home). I'm a 
+                software devoloper (he/him pronouns) who lives in Portland, Or.
+            </div>
+            <div>
+                I love playing music, and I play several instruments. My 2nd favorite
+                movie is the original 1954 Godzilla.
+            </div>
+            <div>
+                Please use this site as a helpful resource to learn about me!
+            </div>
+        </section>
+    `;
+    }
+}
+
+export default Home;
