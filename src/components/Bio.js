5e15c1107fe928c606b2ba11c2a421901fe11e81
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-09 03:37:41 -0700

    added content to bio component

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,7 +3,26 @@ import Component from './Component.js';
 class Bio extends Component {
     renderTemplate() {
         return /*html*/`
-        <div class="hidden">Hi I’m Jon!  Here’s a brief introduction to me, my interests and projects.</div>
+        <secition class="hidden bio-comp">
+            <div>
+                Hi I’m Jon!  Here’s a brief introduction to me, my interests and 
+                projects.
+            </div>
+            <div>
+                I am have been coding in one capacity or another since 1996, although 
+                I've recently been taking my coding skills to the next level!  I like 
+                working in JavasScript and Python the best.
+            </div>
+            <div>
+                Professionaly, I've been working with technology
+                for a while now, most recently in a hosptial as a BMET (medecal 
+                equipment technician) conducting serviceing and repair of all types
+                of medical devices.   
+            </div>
+            <div>
+                I am especialy interested in getting into DevOps.
+            </div>
+        </secition>
     `;
     }
 }
