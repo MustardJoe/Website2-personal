7d7f48c2585e19d22f162e571bb679a81eef142d
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-13 11:12:41 -0700

    created loading comp, will need to impliment and add img

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,17 @@
+import Component from '../Component.js';
+
+class Loading extends Component {
+    renderTemplate() {
+        const loading = this.props.loading;
+        if(!loading) {
+            return '<div></div>';
+        }
+        return /*html*/ `
+            <div class="loading">
+                <img src="../assets/loading4.gif">
+            </div>
+        `;
+    }
+}
+
+export default Loading;
