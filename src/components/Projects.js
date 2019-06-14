3b3fca1170f718046410393fb18928d584fbebae
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-13 17:06:47 -0700

    added moddo link in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,8 +5,14 @@ class Projects extends Component {
         return /*html*/`
         <section class="hidden">
             <div>
+                <a href="https://moddo.netlify.com/">Moddo: Inspire and Advice</a>
+                Moddo is a project I worked on as part of an exercise in using Firebase
+                database system.  It gives you a bit of random adivce and then has a todo
+                list app.
+            </div>
+            <div>
                 Check out my <a href="https://github.com/MustardJoe">GitHub</a> 
-                to see my projects.
+                to see the rest of my projects.
             </div>        
         </section>
     `;
