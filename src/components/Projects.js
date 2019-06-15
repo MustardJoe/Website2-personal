470257a9a80791ec7254b87e15bcfb2e1cc513e6
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-15 11:46:41 -0700

    added spacing similar to other pages

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,16 +3,16 @@ import Component from './Component.js';
 class Projects extends Component {
     renderTemplate() {
         return /*html*/`
-        <section class="hidden">
+        <section class="hidden projects-comp">
             <div>
-                <a href="https://moddo.netlify.com/">Moddo: Inspire and Advice</a>
-                Moddo is a project I worked on as part of an exercise in using Firebase
-                database system.  It gives you a bit of random adivce and then has a todo
-                list app.
+                <a href="https://moddo.netlify.com/" target="_blank">Moddo: Advice and 
+                Inspire</a> Moddo is a project I worked on as part of an exercise
+                in using Firebase database system.  It gives you a bit of random 
+                adivce and then has a todo list app.
             </div>
             <div>
-                Check out my <a href="https://github.com/MustardJoe">GitHub</a> 
-                to see the rest of my projects.
+                Check out my <a href="https://github.com/MustardJoe" target="_blank">
+                GitHub</a>to see the rest of my projects.
             </div>        
         </section>
     `;
