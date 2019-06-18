f25142f4d90b0100c50a59fe59316763738e59f8
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-17 23:47:10 -0700

    still not doing what I want but dot dot dot

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,12 @@ class Footer extends Component {
     renderTemplate() {
         return /*html*/`
         <div class="footer">
-            You've made it to the bottom of the page! Congratulations!
-            © Jon Forney 2019
+            <div>
+                You've made it to the bottom of the page! Congratulations!
+            </div>
+            <section>
+                © Jon Forney 2019
+            </section>
         </div>
     `;
     }
