f36f1e2e6f45ec401c54920c78601c0e99980b85
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-18 22:50:05 -0700

    initial refactor saves lots of space

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,41 +25,77 @@ class NavBar extends Component {
         const linksDOM = links.render();
         dom.appendChild(linksDOM);
 
-        bio1[0].addEventListener('click', (event) => {
+        // // use this object to further refactor button event listener calls
+        // const linksObj = {
+        //     link0: 'homeDOM',
+        //     link1: 'bioDOM',
+        //     link2: 'projectsDOM',
+        //     linkk3: 'linksDOM',
+        // };
+
+        function navBarButtons(navButton) {
+
             event.preventDefault();
-            homeDOM.classList.remove('hidden');
+            homeDOM.classList.add('hidden');
             bioDOM.classList.add('hidden');
             projectsDOM.classList.add('hidden');
             linksDOM.classList.add('hidden');
+            navButton.classList.remove('hidden');
+            
+        }
 
+        bio1[0].addEventListener('click', () => {
+            navBarButtons(homeDOM);
+        });     
+        
+        // bio1[0].addEventListener('click', (event) => {
+        //     event.preventDefault();
+        //     homeDOM.classList.remove('hidden');
+        //     bioDOM.classList.add('hidden');
+        //     projectsDOM.classList.add('hidden');
+        //     console.log(bio1[0].value);
+        //     linksDOM.classList.add('hidden');
+
+        // });
+
+        bio1[1].addEventListener('click', () => {
+            navBarButtons(bioDOM);
         });
 
-        bio1[1].addEventListener('click', (event) => {
-            event.preventDefault();
-            bioDOM.classList.remove('hidden');
-            homeDOM.classList.add('hidden');
-            projectsDOM.classList.add('hidden');
-            linksDOM.classList.add('hidden');
+        // bio1[1].addEventListener('click', (event) => {
+        //     event.preventDefault();
+        //     bioDOM.classList.remove('hidden');
+        //     homeDOM.classList.add('hidden');
+        //     projectsDOM.classList.add('hidden');
+        //     linksDOM.classList.add('hidden');
 
+        // });
+
+        bio1[2].addEventListener('click', () => {
+            navBarButtons(projectsDOM);
         });
 
-        bio1[2].addEventListener('click', (event) => {
-            event.preventDefault();
-            projectsDOM.classList.remove('hidden');
-            bioDOM.classList.add('hidden');
-            homeDOM.classList.add('hidden');
-            linksDOM.classList.add('hidden');
+        // bio1[2].addEventListener('click', (event) => {
+        //     event.preventDefault();
+        //     projectsDOM.classList.remove('hidden');
+        //     bioDOM.classList.add('hidden');
+        //     homeDOM.classList.add('hidden');
+        //     linksDOM.classList.add('hidden');
 
+        // });
+
+        bio1[3].addEventListener('click', () => {
+            navBarButtons(linksDOM);
         });
 
-        bio1[3].addEventListener('click', (event) => {
-            event.preventDefault();
-            linksDOM.classList.remove('hidden');
-            bioDOM.classList.add('hidden');
-            homeDOM.classList.add('hidden');
-            projectsDOM.classList.add('hidden');
+        // bio1[3].addEventListener('click', (event) => {
+        //     event.preventDefault();
+        //     linksDOM.classList.remove('hidden');
+        //     bioDOM.classList.add('hidden');
+        //     homeDOM.classList.add('hidden');
+        //     projectsDOM.classList.add('hidden');
 
-        });
+        // });
 
         return dom;
     }
@@ -68,7 +104,7 @@ class NavBar extends Component {
             <div class="nav-bar">
                 <ul>
                     <li>
-                        <a class="bio1" href="./">Home</a>
+                        <a class="bio1" href="./" value="home">Home</a>
                     </li> 
                     <li>
                         <a class="bio1" href="./">Bio</a>
