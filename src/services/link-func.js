dbef1e7e8006ca66742d755dd3ef3a35538c0056
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-18 23:11:06 -0700

    might not get this part to work this way

--- src/services/link-func.js
+++ src/services/link-func.js
@@ -1,12 +1,12 @@
-function navBarButtons(navButton) {
+// function navBarButtons(navButton) {
 
-    event.preventDefault();
-    homeDOM.classList.add('hidden');
-    bioDOM.classList.add('hidden');
-    projectsDOM.classList.add('hidden');
-    linksDOM.classList.add('hidden');
-    navButton.classList.remove('hidden');
+//     event.preventDefault();
+//     homeDOM.classList.add('hidden');
+//     bioDOM.classList.add('hidden');
+//     projectsDOM.classList.add('hidden');
+//     linksDOM.classList.add('hidden');
+//     navButton.classList.remove('hidden');
     
-}
+// }
 
 // will have to work on this tommorrow
