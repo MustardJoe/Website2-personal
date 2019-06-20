97d4edcc857789bd1b04de55a1d6763feebc5d08
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-20 03:00:22 -0700

    tweaked bio still needs more work

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,22 +5,23 @@ class Bio extends Component {
         return /*html*/`
         <secition class="hidden bio-comp">
             <div>
-                Hi I’m Jon!  Here’s a brief introduction to me, my interests and 
-                projects.
+                Hi I’m Jon!  Here’s a brief introduction to who I am:
             </div>
             <div>
-                I am have been coding in one capacity or another since 1996, although 
-                I've recently been taking my coding skills to the next level!  I like 
-                working with JavasScript and Python the best.
+                I was around for the dawn of the internet.  I began coding at
+                a relatively young age, and as a youth I wrote several Geocities pages.
+                In fact, I have have been coding since 1996. Recently, I've been taking my 
+                coding skills to the next level!  I like working with JavasScript and 
+                Python the best.
             </div>
             <div>
-                Professionaly, I've been working with technology
-                for a while, most recently in a hosptial as a BMET (bio-medical 
-                equipment technician) conducting serviceing and repair of all types
-                of medical devices.   
+                Before transitioning into software, I spent many years working with a 
+                technical focus on hardware, most recently in a hosptial as a BMET 
+                (bio-medical equipment technician) conducting serviceing and repair of all 
+                types of medical devices, in laboratory and field settings.   
             </div>
             <div>
-                I am especialy interested in getting into DevOps.
+                I am especialy interested in back-end and DevOps.
             </div>
         </secition>
     `;
