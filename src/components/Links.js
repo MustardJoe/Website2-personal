4eb0dd27992534bde859fc1ca4ac687ba5622e1f
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-22 14:56:08 -0700

    added spacer div and twitter handle link and img alts

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,12 +4,18 @@ class Links extends Component {
     renderTemplate() {
         return /*html*/`
             <div class="hidden soc-media">
+                <div class="spacer-div"></div>
+                <div class="twitter-tag">
+                    <a href="https://twitter.com/GodzillaJonny" target="_blank">
+                        @GodzillaJonny
+                    </a>
+                </div>
                 <div class="img-links">
                     <a href="https://github.com/MustardJoe" target="blank">
-                        <img src="./assets/github-grn2.png">
+                        <img src="./assets/github-grn2.png" alt="GitHub icon">
                     </a>
                     <a href="https://www.linkedin.com/in/jonathan-forney-52431a79/" target="blank">
-                        <img src="./assets/linkedin-grn.png">
+                        <img src="./assets/linkedin-grn.png" alt="linked in icon">
                     </a>
                 </div>
             </div>
