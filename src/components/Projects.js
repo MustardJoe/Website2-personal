746cddf53d63ad142688f499e58509dc9c50b59d
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-22 14:56:58 -0700

    added link to deployed simpsons api quotes page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,7 @@ class Projects extends Component {
     renderTemplate() {
         return /*html*/`
         <section class="hidden projects-comp">
+            <div class="spacer-div"></div>
             <div>
                 <a href="https://moddo.netlify.com/" target="_blank">Moddo: Advice and 
                 Inspire</a> Moddo is a project I worked on as part of an exercise
@@ -11,12 +12,17 @@ class Projects extends Component {
                 adivce and then has a todo list app.
             </div>
             <div>
+                <a href="https://confident-shirley-560be5.netlify.com/index.html" target="_blank">
+                Jonny's Simpsons Quotes!</a> A project I created that pulls data from a Simpsons API
+                and displays that data as character images and quotes.
+            </div>
+            <div>
                 Check out my <a href="https://github.com/MustardJoe" target="_blank">
                 GitHub</a>to see the rest of my projects.
             </div>
             <div class="img-links">
                 <a href="https://github.com/MustardJoe" target="blank">
-                    <img src="./assets/github-grn2.png">
+                    <img src="./assets/github-grn2.png" alt="GitHub icon">
                 </a>
             </div>        
         </section>
