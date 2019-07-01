479a2bad0b2718a1cf63dcd1d7170e7ae5e102ed
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-30 23:08:58 -0700

    now we get messy- added goals to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,9 +10,13 @@ class Home extends Component {
                 software devoloper (he/him pronouns) who lives in Portland, Or.
             </div>
             <div>
-                I was around for the dawn of the internet (jk, but not really :-) <-- see, i remember when this is what a smiley face emoji lookied like!) As a youth 
+                I was around for the dawn of the internet (jk, but not really :-) <-- see,
+                 i remember when this is what a smiley face emoji lookied like!) As a youth 
                 I wrote several Geocities pages.  In fact, I have have been coding since 
                 1996. Recently, I've been taking my coding skills to the next level!
+                make website a game where you level up, using something similar to star bbs.
+                track experience and update avatar and minor style or decoration changes.
+                css gradient
             </div>
             <div>
                 I love playing music, and I play several instruments. My 3rd favorite
