b6ac88a8d0f0bf28a39419fabfdfe13209ea61ec
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-04 18:34:07 -0700

    added joliliah to projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,13 @@ class Projects extends Component {
         <section class="hidden projects-comp">
             <div class="spacer-div"></div>
             <div>
+                <a href="https://happy-lovelace-66fb73.netlify.com/auth.html" target="blank">
+                I Believe In URL</a> Is a URL shortening service (similar to bit.ly) that makes use
+                of MongoDB to take a long URL provided by a user and return a shorter 
+                alternative for use in places like Twitter or elsewhere when typing 
+                out an enourmouse link is unwieldly/not possible. 
+            </div>
+            <div>
                 <a href="https://moddo.netlify.com/" target="_blank">Moddo: Advice and 
                 Inspire</a> Moddo is a project I worked on as part of an exercise
                 in using Firebase database system.  It gives you a bit of random 
